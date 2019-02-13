3cbc46eb0146d77586a29413fab294dd11e03f56
an4tman <kewebber@gmail.com>
2019-02-12 22:59:44 -0500

    fixed indentation and spacing in bot

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@
 bot selects the lie from any number of positional arguments
 """
 
+
 def longest(*args):
-     # max() takes a second argument, a key function, in this case the length of the string
-     return max(args, key=len)
+    # max() takes a second argument, a key function,
+    # in this case the length of the string
+    return max(args, key=len)
+
 
 def shortest(*args):
     # same as max() example above
