ac94f93fd2c175fcdbe4f80ce951bfb264e82d0e
an4tman <kewebber@gmail.com>
2019-02-12 22:06:41 -0500

    add test bot

---
+++ bot.py
@@ -0,0 +1,11 @@
+"""
+bot selects the lie from any number of positional arguments
+"""
+
+def longest(*args):
+     # max() takes a second argument, a key function, in this case the length of the string
+     return max(args, key=len)
+
+def shortest(*args):
+    # same as max() example above
+    return min(args, key=len)
