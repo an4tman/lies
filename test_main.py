79c8469bafd5a6874ef9a200713e1cd870c4aa16
an4tman <kewebber@gmail.com>
2019-02-13 13:40:22 -0500

    added test_main

---
+++ test_main.py
@@ -0,0 +1,24 @@
+"""
+unit tests for the bot library
+"""
+
+#import main
+
+with open("test_data/true", "r") as true_fo:
+    true_input = true_fo.readlines()
+true_input = [i.strip() for i in true_input]
+
+with open("test_data/false", "r") as false_fo:
+    false_input = false_fo.readlines()
+false_input = [i.strip() for i in false_input]
+
+with open("test_data/misspelled", "r") as misspelled_fo:
+    misspelled_input = misspelled_fo.readlines()
+misspelled_input = [i.strip() for i in misspelled_input]
+
+with open("test_data/evil", "r") as evil_fo:
+    evil_input = evil_fo.readlines()
+evil_input = [i.strip() for i in evil_input]
+
+print(true_input)
+print(false_input)
