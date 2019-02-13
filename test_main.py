3242dffc2445e46a2164d3f8d041a04a72cfe655
Lisa Jones <jones.lisa.94@gmail.com>
2019-02-13 14:14:17 -0500

    Refactored tests to use pytest library

--- test_main.py
+++ test_main.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+
 """
 unit tests for the bot library
 """
 
-#import main
+import bot
+import pytest
 
 with open("test_data/true", "r") as true_fo:
     true_input = true_fo.readlines()
@@ -20,5 +23,26 @@ with open("test_data/evil", "r") as evil_fo:
     evil_input = evil_fo.readlines()
 evil_input = [i.strip() for i in evil_input]
 
-print(true_input)
-print(false_input)
+# print(true_input)
+# print(false_input)
+
+@pytest.mark.parametrize("us, expected", [
+    ("What is my name?", False),
+    ("Foo is my name.", True),
+])
+def test_val_sentence(us, expected):
+    """Test sentence validation."""
+    assert bot.validate_sentence(us) == expected
+
+
+@pytest.mark.parametrize("us, expected", [
+    ("A"*141, False),
+    ("A"*140, True),
+    ("A"*139, True),
+])
+def test_val_length(us, expected):
+    """Test length validation."""
+    assert bot.validate_length(us) == expected
+
+
+    
